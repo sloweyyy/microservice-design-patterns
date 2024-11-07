ba86fd801f9e3c8c7f68e6bc70f2878284b6fc78
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2024-11-07 21:06:37 +0700

    feat(api-gateway): Add initial API files and configurations for Basket, Catalog, Discount, and Ordering services

---
+++ microservices-4-patterns/Infrastructure/EventBus.Messages/Common/EventBusConstant.cs
@@ -0,0 +1,8 @@
+﻿namespace EventBus.Messages.Common
+{
+    public class EventBusConstant
+    {
+        public const string BasketCheckoutQueue = "basketcheckout-queue";
+        public const string BasketCheckoutQueueV2 = "basketcheckout-queue-v2";
+    }
+}
