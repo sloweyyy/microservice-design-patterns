0fa33c567d17c99feebff464459b393ec9f5c2e0
Truong Le Vinh Phuc <truonglevinhphuc2006@gmail.com>
2025-01-14 17:45:42 +0700

    chore: Remove unused files and configurations from client and services

--- microservices-4-patterns/Services/Ordering/Ordering.Core/Repositories/IOrderRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Ordering.Core.Entities;
-
-namespace Ordering.Core.Repositories;
-
-public interface IOrderRepository : IAsyncRepository<Order>
-{
-    Task<IEnumerable<Order>> GetOrdersByUserName(string userName);
-}
